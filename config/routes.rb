ecea82071cac95e6b8ef808521ec5d3f539de2f6
Roger Lam <mrlamroger@gmail.com>
2013-03-26 16:31:32 -0700

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Website::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/blog"
+  root to: 'static_pages#home'
+
+  match '/about',   to: 'static_pages#about'
+  match '/blog', to: 'static_pages#blog'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
